990f0a7c0da09559b19cb701b10ceaac75813d8b
ahmed14 <ahmed.amka@fci.suezuni.edu.eg>
2022-10-04 13:37:14 +0200

    first commit

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-# eb init udagram-api --region us-east-1 --platform node.js
-# eb create --sample udagram-api-dev
-# eb use udagram-api-dev
-# eb setenv POSTGRES_HOST="database-1.cguyu01hu7lw.us-east-1.rds.amazonaws.com" DB_PORT=5432 PORT=3000 POSTGRES_PASSWORD="1234" POSTGRES_USERNAME="postgres" RDS_DIALECT="postgres" POSTGRES_DB="udagram" URL="http://udagram-api-dev.eba-7ffeir2f.us-east-1.elasticbeanstalk.com" JWT_SECRET="hello world iam udagram" AWS_REGION="us-east-1" AWS_PROFILE1="default" AWS_BUCKET="arn:aws:s3:::udagram-kotb" AWS_ACCESS_KEY_ID="AKIAVAA6U5UG7PHZYPWM" AWS_SECRET_ACCESS_KEY="i3lYpTw/UsUGwcf4tgZlRCW3oPy+ORa1bHNM8IsT"
-# eb deploy udagram-api-dev
+eb init $App --region $AWS_REGION --platform node.js
+eb create --sample udagram-api-dev
+eb use udagram-api-dev
+eb setenv POSTGRES_HOST="database-1.cguyu01hu7lw.us-east-1.rds.amazonaws.com" DB_PORT=5432 PORT=3000 POSTGRES_PASSWORD="1234" POSTGRES_USERNAME="postgres" RDS_DIALECT="postgres" POSTGRES_DB="udagram" URL="http://udagram-api-dev.eba-7ffeir2f.us-east-1.elasticbeanstalk.com" JWT_SECRET="hello world iam udagram" AWS_REGION="us-east-1" AWS_PROFILE1="default" AWS_BUCKET="arn:aws:s3:::udagram-kotb" AWS_ACCESS_KEY_ID="AKIAVAA6U5UG7PHZYPWM" AWS_SECRET_ACCESS_KEY="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"
+eb deploy udagram-api-dev
