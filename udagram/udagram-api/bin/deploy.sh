8bb4c3785fa9f94f1ef22a2d49288a9b9c52fd48
ahmed14 <ahmed.amka@fci.suezuni.edu.eg>
2022-10-06 10:11:09 +0200

    first commit

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,4 @@
 eb init $App --region $AWS_REGION --platform node.js
 eb create --sample udagram-api-dev-3
-eb use udagram-api-dev-3
-eb setenv POSTGRES_HOST="database-1.cguyu01hu7lw.us-east-1.rds.amazonaws.com" DB_PORT=5432 PORT=3000 POSTGRES_PASSWORD="1234" POSTGRES_USERNAME="postgres" RDS_DIALECT="postgres" POSTGRES_DB="udagram" JWT_SECRET="hello world iam udagram" 
+eb use udagram-api-dev-3 
 eb deploy udagram-api-dev-3
