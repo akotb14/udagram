e66ed0bf74798ea44786d73d4608648d4747819e
ahmed14 <ahmed.amka@fci.suezuni.edu.eg>
2022-10-08 10:14:39 +0200

    first-commit

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init $App --region $AWS_REGION --platform node.js
 eb create --sample $environment
 eb use $environment 
-eb setenv $PORT
+eb setenv $PORT $AWS_ACCESS_KEY_ID $AWS_BUCKET $AWS_DEFAULT_REGION $AWS_REGION $AWS_SECRET_ACCESS_KEY $POSTGRES_HOST $DB_PORT $POSTGRES_PASSWORD $POSTGRES_USERNAME $POSTGRES_DB $JWT_SECRET $URL $AWS_PROFILE 
 eb deploy $environment
